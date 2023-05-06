51cece8e17d0d18bf65742587e3b6c13514a90db
seonyo <jsy6535@naver.com>
2023-05-06 23:30:32 +0900

    문자열 돌리기

---
+++ Coding_Test/src/programas/D2023_05_06.java
@@ -0,0 +1,14 @@
+package programas;
+
+import java.util.Scanner;
+
+public class D2023_05_06 {
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+	        String a = sc.next();
+	        for(int i=0; i<a.length(); i++){
+	            System.out.println(a.charAt(i) + " ");
+	        }
+	}
+}
